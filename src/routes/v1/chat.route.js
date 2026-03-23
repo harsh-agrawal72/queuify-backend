e4edbe3f270844513fd34ea6aceb23dda70adab4
harsh-agrawal72 <harsagrawal7270@gmail.com>
2026-03-23 20:15:09 +0530

    changes

--- src/routes/v1/chat.route.js
+++ src/routes/v1/chat.route.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { auth } = require('../../middlewares/auth.middleware');
+const auth = require('../../middlewares/auth');
 const chatController = require('../../controllers/chat.controller');
 
 const router = express.Router();
