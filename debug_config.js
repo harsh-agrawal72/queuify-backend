0d10e78f685d00cefe6ca000ba27b554b9ccd14a
Harsh Agrawal <harsagrawal7270@gmail.com>
2026-04-01 12:44:49 +0530

    changes

--- debug_config.js
+++
@@ -1,4 +0,0 @@
-const config = require('./src/config/config');
-console.log('Email Enabled Config:', config.email.enabled);
-console.log('Type of Email Enabled:', typeof config.email.enabled);
-console.log('ENABLE_EMAIL from process.env:', process.env.ENABLE_EMAIL);
--- schema_check.js
+++
@@ -1,5 +0,0 @@
-const { pool } = require('./src/config/db'); 
-pool.query('SELECT column_name, data_type FROM information_schema.columns WHERE table_name = \'appointments\'')
-.then(res => console.log(res.rows))
-.catch(console.error)
-.finally(() => pool.end());
